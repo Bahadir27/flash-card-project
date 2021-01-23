bb9534d4eb82bac085664946da93cc112595003c
Bahadir27 <b.ucer27@gmail.com>
2021-01-23 12:10:16 +0100

    Flip Card and Timer
    after 3s the card gets flipped

--- main.py
+++ main.py
@@ -8,12 +8,27 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/german_words.csv")
 data_dict = data.to_dict(orient="records")
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(data_dict)
-    canvas.itemconfig(card_title, text="German")
-    canvas.itemconfig(card_word, text=current_card["German"])
+    canvas.itemconfig(card_title, text="German", fill="black")
+    canvas.itemconfig(card_word, text=current_card["German"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, flip_card)
+
+
+# ---------------------- BACKSIDE of CARD ----------------------
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_card["English"])
 
 
 # ---------------------- USER INTERFACE ----------------------
@@ -22,9 +37,12 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 40, "italic"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
